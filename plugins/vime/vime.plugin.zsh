5df487e7a4ae60fecf4e55aefc4b4aa4af4fd5d7
Kevin Druelle <kdruelle@opsise.com>
2016-10-14 09:15:28 +0200

    Plugin vim encrypt

---
+++ plugins/vime/vime.plugin.zsh
@@ -0,0 +1,26 @@
+
+
+
+init () {
+    if [ ! -f ~/.vimenc ];then
+        echo "source ~/.vimrc"    > ~/.vimenc
+        echo "set nobackup"      >> ~/.vimenc
+        echo "set noswapfile"    >> ~/.vimenc
+        echo "set nowritebackup" >> ~/.vimenc
+        echo "set cm=blowfish"   >> ~/.vimenc
+    fi
+}
+
+
+
+vime () {
+    if [ -f $1 ]; then
+        vim -u ~/.vimenc "$1"
+    else
+        vim -u ~/.vimenc -x "$1"
+    fi
+}
+
+
+init()
+
